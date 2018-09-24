2fd2b59653eefe1bc10c03fa67760a7c070b56d4
Zakaria Talhami <Zakaria.a.a.talhami@gmail.com>
2018-09-24 22:41:28 +0300

    init

---
+++ src/main/java/com/bzu/mimir/MimirApplication.java
@@ -0,0 +1,12 @@
+package com.bzu.mimir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimirApplication.class, args);
+	}
+}
